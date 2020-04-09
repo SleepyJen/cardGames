c5a5ab97fbbabac53e7c8195ea4d9deba664e45c
SleepyJen <jennifer.ni0924@gmail.com>
2020-04-09 01:37:59 -0700

    added user model

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    firstName: {
+        type: String,
+        required: "Must include a first name"
+    },
+
+    lastName: {
+        type: String,
+        default: ''
+    },
+
+    password: {
+        type: String,
+        required: 'must include a password'
+    },
+
+    players: {
+        type: [],
+        default: []
+    }
+});
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 8000 || process.env.PORT;
 const Color = require('colors');
+const path = require('path');
 
 const mongoose = require('mongoose');
 const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/QuarentineBoardGame";
@@ -19,6 +20,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 app.listen(PORT, () => {
     console.log('----------------------------'.rainbow);
